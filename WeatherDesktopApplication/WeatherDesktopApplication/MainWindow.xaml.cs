55810f0d8b934887fedf1118068b023e678db202
george.dulica <dulica_george_10@yahoo.com>
2025-01-24 14:50:09 +0200

    Change location windows

--- WeatherDesktopApplication/WeatherDesktopApplication/MainWindow.xaml.cs
+++ WeatherDesktopApplication/WeatherDesktopApplication/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WeatherDesktopApplication
 {
@@ -19,6 +11,19 @@ namespace WeatherDesktopApplication
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var location = LocationTextBox.Text;
+            if (string.IsNullOrEmpty(location))
+            {
+            }
         }
     }
 }
